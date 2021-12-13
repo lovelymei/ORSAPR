21b52da41a3d2f24ebab3dd037c0c4b013766a76
lovelymei <meimei32907@gmail.com>
2021-12-13 09:42:58 +0700

    подправила сеттеры

--- ChessRook/ChessRook/RookInfo.cs
+++ ChessRook/ChessRook/RookInfo.cs
@@ -9,11 +9,80 @@ namespace ChessRook
         private int _upperBaseDiameter;
         private int _lowerBaseDiameter;
 
-        public int FullHeight { get { return _fullHeight; } set { _fullHeight = value; } }
-        public int UpperBaseHeight { get { return _upperBaseHeight; } set { _upperBaseHeight = value; } }
-        public int LowerBaseHeight { get { return _lowerBaseHeight; } set { _lowerBaseHeight = value; } }
-        public int UpperBaseDiameter { get { return _upperBaseDiameter; } set { _upperBaseDiameter = value; } }
-        public int LowerBaseDiameter { get { return _lowerBaseDiameter; } set { _lowerBaseDiameter = value; } }
+        public int FullHeight { 
+            get 
+            { 
+                return _fullHeight;
+            } 
+            set 
+            { 
+                if ((value < 10000) && (value > 10))
+                {
+                    _fullHeight = value;
+                }
+                else
+                {
+                   
+                }
+            } 
+        }
+
+        public int UpperBaseHeight { 
+            get
+            { 
+                return _upperBaseHeight;
+            } 
+            set 
+            {
+                if ((value < 150) && (value > 2))
+                {
+                    _upperBaseHeight = value;
+                }
+            } 
+        }
+        public int LowerBaseHeight { 
+            get 
+            { 
+                return _lowerBaseHeight; 
+            } 
+            set 
+            {
+                if ((value < 100) && (value > 3))
+                {
+                    _lowerBaseHeight = value;
+                }
+            } 
+        }
+
+        public int UpperBaseDiameter 
+        {
+            get 
+            { 
+                return _upperBaseDiameter; 
+            } 
+            set 
+            {
+                if ((value < 100) && (value > 3))
+                {
+                    _upperBaseDiameter = value;
+                }
+            } 
+        }
+
+        public int LowerBaseDiameter 
+        { 
+            get 
+            { 
+                return _lowerBaseDiameter; 
+            } 
+            set 
+            {
+                if ((value < 500) && (value > 5))
+                {
+                    _lowerBaseDiameter = value;
+                }
+            } 
+        }
 
         public RookInfo() { }
     }
