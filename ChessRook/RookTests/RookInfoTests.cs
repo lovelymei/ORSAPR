d41fc718cbced8ab4d2e7243d0e0d36b753ec889
lovelymei <meimei32907@gmail.com>
2021-12-18 13:44:36 +0700

    мелочь всякая

---
+++ ChessRook/RookTests/RookInfoTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RookTests
+{
+    [TestFixture]
+    class RookInfoTests
+    {
+        [Test]
+        public void Validation_CorrectValues_ReturnsTrue()
+        {
+            var value = 5;
+            var min = 0;
+            var max = 10;
+
+        }
+    }
+}
